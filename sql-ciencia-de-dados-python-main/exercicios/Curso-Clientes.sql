ec2b765a461dc1daa76cbff3a4a61942c557c76e
Thaynara Alves <thaynaraalves1915@gmail.com>
2026-05-25 22:11:25 -0300

    Adicionando exercício criação de tabela clientes

--- sql-ciencia-de-dados-python-main/exercicios/Curso-Clientes.sql
+++ sql-ciencia-de-dados-python-main/exercicios/Curso-Clientes.sql
@@ -1,11 +1,11 @@
 CREATE TABLE Clientes(
 id INT not null IDENTITY (1,1),
--- id int not nuul, significa que é um identificador unico e precisa ser preenchido.
--- IDNTITY é um contador de cadastro.
+-- id int not nuul, significa que ï¿½ um identificador unico e precisa ser preenchido.
+-- IDNTITY ï¿½ um contador de cadastro.
 -- (1,1) vai ser contado de 1 em 1, ex : 1,2,3 ...
  nome varchar (30) not null,
  nascimento date not null, 
- peso decimal(5,2) not null,  -- DECIMAL é usado para valores numéricos exatos
+ peso decimal(5,2) not null,  -- DECIMAL ï¿½ usado para valores numï¿½ricos exatos
  altura decimal (2,2) not null,
  endereco varchar (20),
 
