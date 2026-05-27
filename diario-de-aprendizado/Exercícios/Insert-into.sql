2e1bdfe564fa3ef2f197022ae96f00d65d949327
Thaynara Alves <thaynaraalves1915@gmail.com>
2026-05-27 20:43:57 -0300

    Aprendendo INSERT INTO

---
+++ diario-de-aprendizado/Exercícios/Insert-into.sql
@@ -0,0 +1,10 @@
+INSERT INTO Clientes
+(nome,nascimento,peso,altura,endereco)
+
+VALUES
+('Maria','1985-02-06',55.0,1.73,'Goiânia'),
+('João','1965-05-25',80.0,1.90,'Brasília'),
+('Marcia','1998-06-19',55.0,1.73,'Goiânia'),
+('Joana','1999-06-29',56.0,1.65,'Goiânia');
+
+SELECT * FROM Clientes;
