5b17c11faab389ef172f7d666024503129175acb
Thaynara Alves <thaynaraalves1915@gmail.com>
2026-06-19 21:27:10 -0300

    Praticando mais um pouco como criar banco de dados,criar tabela,insert into,foreign key e inner join

---
+++ diario-de-aprendizado/Exercícios/PRATICANDO_EXERCÍCIOS.sql
@@ -0,0 +1,69 @@
+create table Secretaria(
+     idSecretaria int primary key identity (1,1),
+     nomeFuncionario varchar(50),
+     telefone varchar(15),
+     email varchar(80)
+ );
+
+  insert into Secretaria(nomeFuncionario,telefone,email)values 
+      ('Alberto','9999-0000','alberto@gmail.com'),
+      ('Matheus','6666-8888','matheuss@gmail.com'),
+      ('Lua','4442-7777','lua@gmail.com'),
+      ('Amora','5555-7777','amora@gmail.com');
+
+   create table Matriculas(
+       idMatriculas int primary key identity(1,1),
+       dataMatricula date,
+       statusMatricula varchar(100)
+  );alter table Matriculas 
+  add idAlunos int;
+
+ alter table Matriculas
+ add idCursos int;
+
+ alter table Matriculas 
+ add idSecretaria int;
+
+ alter table Matriculas 
+ add FOREIGN KEY (idAlunos)
+ references Alunos(idAlunos) ;
+
+ alter table Matriculas 
+ add foreign key (idCursos)
+ references Curso(idCursos) ;
+
+ alter table Matriculas 
+ add foreign key (idSecretaria)
+ references Secretaria(idSecretaria) ;
+
+    insert into Matriculas (dataMatricula,statusMatricula,idAlunos,idCursos,idSecretaria) values 
+   ('2026-06-18', 'Ativa', 3, 1, 1),
+   ('2026-06-18', 'Ativa', 5, 2, 1),
+   ('2026-06-19', 'Pendente', 3, 3, 2),
+   ('2026-06-19', 'Ativa', 4, 1, 2),
+   ('2026-06-20', 'Cancelada', 5, 2, 3),
+   ('2026-06-20', 'Ativa', 6, 4, 3),
+   ('2026-06-21', 'Pendente', 7, 3, 4),
+   ('2026-06-21', 'Ativa', 8, 2, 4);
+ 
+ select nomeAluno,dataMatricula,nomeCurso,nomeFuncionario 
+   from Matriculas 
+   join Alunos
+   on Matriculas.idAlunos = Alunos.idAlunos
+   join Curso
+   on Matriculas.idCursos = Curso.idCursos
+   join Secretaria 
+   on Matriculas.idSecretaria = Secretaria.idSecretaria;
+
+
+  select nomeAluno,dataMatricula,nomeCurso,nomeFuncionario 
+  from Matriculas 
+  join Alunos
+  on Matriculas.idAlunos = Alunos.idAlunos
+  join Curso
+  on Matriculas.idCursos = Curso.idCursos
+  join Secretaria 
+  on Matriculas.idSecretaria = Secretaria.idSecretaria;
+
+
+
--- diario-de-aprendizado/Exercícios/Reforcando-inner-join.sql
+++ diario-de-aprendizado/Exercícios/Reforcando-inner-join.sql
@@ -1,3 +1,4 @@
+
 use EstudosDados;
 
   create table Livros(
@@ -90,10 +91,108 @@ select max(anoPublicaçao)from Livros;
 
 select avg(anoPublicaçao)from Livros;
 
-select nomeAluno, idade, livroFavorito from Alunos
-join Livros on Alunos.livroFavorito = Livros.idLivro;
+   create table Livros (
+       id int primary key identity(1,1),
+       titulo varchar (80),
+       autor varchar(60),
+       categoria varchar(40),
+       anoPublicaçao int
+   );
 
--- Continuação do exercício 18-06
+   insert into Livros (titulo,autor,categoria,anoPublicaçao) values
+   ('SQL Serve para iniciantes','Carlos Silva','Banco de dados',2022),
+   ('Python Essencial','Ana Souza','Programação',2023),
+   ('Power BI Completo','Gustavo Lima','Business Intelligence',2021),
+   ('Excel Avançado','Fernanda Costa','Planilhas',2020),
+   ('Banco de Dados Relacionais','João Pedro','Banco de Dados',2024);
+ 
+   update Livros 
+   set quantidade = 10
+   where titulo = 'Python Essencial';
+
+   update Livros 
+   set quantidade = 6 
+   where titulo = 'Power BI completo';
+
+   update Livros 
+   set quantidade = 12 
+   where titulo = 'Excel Avançado';
+
+   update Livros
+   set quantidade = 5
+   where titulo = 'Banco de dados Relacionais' ;
+
+   alter table Alunos
+   add livroFavorito int;
+
+   ALTER TABLE Alunos
+   ADD CONSTRAINT FK_Alunos_Livros --Tive a juda do chat pois não estava conseguindo fazer o relacionamento das tabelas.
+   FOREIGN KEY (livroFavorito) 
+   REFERENCES Livros(idLivros);
+
+  UPDATE Alunos
+  SET livroFavorito = 1
+  WHERE nomeAluno = 'Agata';
+
+  UPDATE Alunos
+  SET livroFavorito = 2
+  WHERE nomeAluno = 'Vanessa';
+
+  UPDATE Alunos
+  SET livroFavorito = 3
+  WHERE nomeAluno = 'Jaqueline';
+
+  UPDATE Alunos
+  SET livroFavorito = 4
+  WHERE nomeAluno = ' junior';
+
+  UPDATE Alunos
+  SET livroFavorito = 5
+  WHERE nomeAluno = 'Marcelo';
+
+  UPDATE Alunos
+  SET livroFavorito = 6
+  WHERE nomeAluno = 'Maria';
+
+  UPDATE Alunos
+  SET livroFavorito = 7
+  WHERE nomeAluno = 'João';
+
+  UPDATE Alunos
+  SET livroFavorito = 8
+  WHERE nomeAluno = 'Pedro';
+
+  select * from Livros;
+
+  select * from Livros 
+  where anoPublicaçao > 2021;
+
+  select * from Livros 
+  where categoria = 'Banco de dados';
+
+  select * from Livros 
+  where titulo like  'P%';
+
+  select count(*) from Livros;
+
+  select max(anoPublicaçao)from Livros;
+
+  select avg(anoPublicaçao)from Livros;
+
+  select nomeAluno,idade,livroFavorito from Alunos as A
+  join Livros AS L on A.livroFavorito = L.id;
+
+select nomeAluno, idade, livroFavorito from Alunos
+join Livros on Alunos.livroFavorito = Livros.id;
+
+ -- Continuação do exercício 18-06
+
+ select nomeAluno,nomeCurso,livroFavorito 
+ from Alunos
+ join Curso
+ on Alunos.cursoPreferido = Curso.idCursos
+ join Livros
+ on Alunos.livroFavorito = Livros.id;
 
  select nomeAluno,nomeCurso,livroFavorito 
  from Alunos
