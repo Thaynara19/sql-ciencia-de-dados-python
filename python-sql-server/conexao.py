fee09d1761b5dfd2e17c4fa5a6b815c5a218193c
Thaynara Alves <thaynaraalves1915@gmail.com>
2026-07-01 21:23:19 -0300

    Primeira conexão entre Python e SQL Server usando Pandas

--- python-sql-server/conexao.py
+++ python-sql-server/conexao.py
@@ -1,4 +1,27 @@
 import pyodbc
 import pandas as pd
 
-print("Ambiente Python configurado com sucesso!")
+print("1 - Importou bibliotecas")
+
+conexao = pyodbc.connect(
+    "DRIVER={ODBC Driver 17 for SQL Server};"
+    "SERVER=DESKTOP-TR3B0B0\\SQLEXPRESS;"
+    "DATABASE=EstudosDados;"
+    "Trusted_Connection=yes;"
+)
+
+print("2 - Conectou ao banco")
+
+consulta = "SELECT * FROM Alunos"
+
+print("3 - Consulta criada")
+
+df = pd.read_sql(consulta, conexao)
+
+print("4 - DataFrame criado")
+
+print(df)
+
+conexao.close()
+
+print("5 - Conexão encerrada")
