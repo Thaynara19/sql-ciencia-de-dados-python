de5b7d49875357ecc0ccf0ec95491c923a04760f
Thaynara Alves <144859274+Thaynara19@users.noreply.github.com>
2026-07-08 22:00:46 -0300

    Continuação do exercício

--- diario-de-aprendizado/Exercícios/01-criacao-banco/Academia DBA – Desafio 01.sql
+++ diario-de-aprendizado/Exercícios/01-criacao-banco/Academia DBA – Desafio 01.sql
@@ -25,3 +25,41 @@ Questão 5 :
 
 Questão 6 :
        select nome,cidade from Alunos; 
+
+Questão 7 : 
+    select * from Alunos
+    where cidade = ‘Goiânia’;
+
+Questão 8 : 
+      select * from Alunos 
+      Where idade > 20;
+
+Questão 9 : 
+     update Alunos 
+     set idade = 20
+     where nome = ‘Pedro’;
+
+Questão 10 : 
+      delete from Alunos
+      where nome = ‘Lucas’;
+
+Questão 11 : 
+    select count(*) from Alunos;
+
+Questão 12 : 
+     select max(idade) from Alunos;
+
+Questão 13 : 
+      select min(idade) from Alunos;
+
+Questão 14 : 
+      select avg(idade) from Alunos;
+
+Desafio final : 
+       create table Cursos(
+       id int primary key identity(1,1), 
+       nomeCurso varchar (100) not null,
+       cargaHoraria int not null
+);
+
+Final das questões.
